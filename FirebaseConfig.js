cce601052ade71719ffe597a5cb7313a2d5b3a17
chethanam195 <chethana.m195@gmail.com>
2024-11-14 16:07:13 +0530

    Create FirebaseConfig.js

---
+++ FirebaseConfig.js
@@ -0,0 +1,12 @@
+// src/firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCqImlWVCgBzm9NmWLO_PTX0a-XLAe-efQ",
+    authDomain: "fir-project-605f6.firebaseapp.com",
+    projectId: "fir-project-605f6",
+    storageBucket: "fir-project-605f6.appspot.com",
+    messagingSenderId: "574304064991",
+    appId: "1:574304064991:web:1e14e123c2e6084daba06a",
+    measurementId: "G-J6JEL1V6BF"
+};
+
+export default firebaseConfig;
