859ee67d52760bdbd3d74b4c5d28860a633c5362
chethanam195 <chethana.m195@gmail.com>
2024-11-14 16:05:29 +0530

    Update ForgetPassword.js

--- ForgetPassword.js
+++ ForgetPassword.js
@@ -53,16 +53,3 @@ const ForgotPassword = () => {
 export default ForgotPassword;
 
 
-firebaseConfig.js
-// src/firebaseConfig.js
-const firebaseConfig = {
-    apiKey: "AIzaSyCqImlWVCgBzm9NmWLO_PTX0a-XLAe-efQ",
-    authDomain: "fir-project-605f6.firebaseapp.com",
-    projectId: "fir-project-605f6",
-    storageBucket: "fir-project-605f6.appspot.com",
-    messagingSenderId: "574304064991",
-    appId: "1:574304064991:web:1e14e123c2e6084daba06a",
-    measurementId: "G-J6JEL1V6BF"
-};
-
-export default firebaseConfig;
