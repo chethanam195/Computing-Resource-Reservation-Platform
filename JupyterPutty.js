8eb9e0ed484b8cfa780e9d845a357090429b80e2
chethanam195 <chethana.m195@gmail.com>
2024-11-28 10:15:47 +0530

    Update JupyterPutty.js

--- JupyterPutty.js
+++ JupyterPutty.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 
-function JupyterPuttyPage() {
+function JupyterPutty() {
     const [showJupyter, setShowJupyter] = useState(false);
 
-    // Function to launch Jupyter Notebook
     const openJupyter = () => {
-        setShowJupyter(true); // Show the Jupyter Notebook by updating state
+        setShowJupyter(true);
     };
 
-    // Function to open PuTTY using the backend
     const openPutty = () => {
         fetch('http://localhost:5000/api/open-putty')
             .then(response => response.json())
@@ -19,17 +17,11 @@ function JupyterPuttyPage() {
     return (
         <div>
             <h1>Jupyter and PuTTY Page</h1>
-
-            {/* Button to launch Jupyter Notebook */}
             <button onClick={openJupyter}>Open Jupyter Notebook</button>
-
-            {/* Button to open PuTTY */}
             <button onClick={openPutty}>Open PuTTY</button>
-
-            {/* Conditional rendering of the Jupyter Notebook iframe */}
             {showJupyter && (
                 <iframe
-                    src="http://localhost:8888" // Assuming Jupyter Notebook is running locally on port 8888
+                    src="http://localhost:8888"
                     title="Jupyter Notebook"
                     style={{ width: '100%', height: '600px', border: 'none' }}
                 />
@@ -38,4 +30,4 @@ function JupyterPuttyPage() {
     );
 }
 
-export default JupyterPuttyPage;
+export default JupyterPutty;
