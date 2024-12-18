1c029573f69713ad7a3bb0893321ab902886ce6b
chethanam195 <chethana.m195@gmail.com>
2024-12-18 10:52:07 +0530

    Create Overview.js

---
+++ Overview.js
@@ -0,0 +1,21 @@
+// Overview.js
+import React from 'react';
+import banner from './assets/banner.jpg';
+
+const Overview = () => {
+    return (
+        <div className="overview-section">
+            <h2>Overview of Our Platform</h2>
+            <p>
+                Welcome to the Computing Resource Booking Platform! Our platform allows you to easily
+                access and book cloud computing resources for your projects.
+            </p>
+            <p>
+                We offer flexible plans and services to suit your needs, whether for short-term or
+                long-term use. Explore our offerings and get started today!
+            </p>
+        </div>
+    );
+};
+
+export default Overview;
